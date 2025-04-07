16450c9a350f121dc6955d566804ad40103d4ac1
tsori <tsori.kislev@mail.huji.acil>
2025-04-07 17:31:20 +0300

    b

--- merge_graphs.py
+++ merge_graphs.py
@@ -1,9 +1,7 @@
+import json
 import os
 import sys
-import random
-from collections import defaultdict
 import networkx as nx
-from networkx.readwrite import json_graph
 import matplotlib.pyplot as plt
 from complex_graph import graph, SubunitName, SubunitInfo
 from typing import List
@@ -15,7 +13,7 @@ def overlap(v1: SubunitInfo, v2: SubunitInfo) -> bool:
     return any(chain in v2.chain_names for chain in v1.chain_names) and not (v1.end < v2.start or v2.end < v1.start)
 
 
-def merge_graphs(graphs: List[nx.Graph]) -> nx.Graph:
+def merge_graphs(graphs: List[nx.Graph],folder) -> nx.Graph:
     print("Merging graphs")
     """Merge nodes with overlapping indices in the same chain across multiple graphs."""
     merged_graph = nx.Graph()
@@ -47,28 +45,36 @@ def merge_graphs(graphs: List[nx.Graph]) -> nx.Graph:
     # Step 3: Merge nodes in each component
     print("Merging nodes")
     merged_nodes = {}
+    output_subunits={}
     node_mapping = {}
 
     for component in connected_components:
         subunits = [all_nodes[name] for name in component]
 
         # Merge properties
-        merged_name = SubunitName(f"{subunits[0].name[0:-1]}: {subunits[0].start}-{subunits[0].end}")
+        merged_name = subunits[0].chain_names[0]
         merged_chains = sorted(set(chain for subunit in subunits for chain in subunit.chain_names))
         merged_start = min(subunit.start for subunit in subunits)
         merged_end = max(subunit.end for subunit in subunits)
-        # merged_sequence = "".join(subunit.sequence for subunit in subunits)  # Optional
-
+        merged_sequence = [''] * (merged_end - merged_start)
+        for subunit in subunits:
+            merged_sequence[subunit.start-merged_start:subunit.end-merged_start] = list(subunit.sequence)
+        merged_sequence = "".join(merged_sequence)
         merged_subunit = SubunitInfo(
             name=merged_name,
             chain_names=merged_chains,
             start=merged_start,
             end=merged_end,
-            sequence=''
+            sequence=merged_sequence,
         )
-
+        dict_subunit ={
+            "name": merged_name,
+            "chain_names": merged_chains,
+            "start_res": merged_start,
+            "sequence": merged_sequence,
+        }
         merged_nodes[merged_name] = merged_subunit
-
+        output_subunits[merged_name] = dict_subunit
         # Map original nodes to merged node name
         for name in component:
             node_mapping[name] = merged_name
@@ -83,15 +89,17 @@ def merge_graphs(graphs: List[nx.Graph]) -> nx.Graph:
             merged_u = node_mapping[u]
             merged_v = node_mapping[v]
             merged_graph.add_edge(merged_u, merged_v)
+    with open(os.path.join(folder,"redefine_subunits2.json"),'w') as f:
+        json.dump(output_subunits, f, indent=2)
 
     return merged_graph
 
-def create_graphs_from_folder(folder):
-    print(f"Creating graphs from folder {folder}")
+def create_graphs_from_folder(folder_path):
+    print(f"Creating graphs from folder {folder_path}")
     graphs = []
     # Iterate over all folders in the folder
-    for item in os.listdir(folder):
-        item_path = os.path.join(folder, item)
+    for item in os.listdir(folder_path):
+        item_path = os.path.join(folder_path, item)
         if os.path.isdir(item_path):
             print(f"Processing {item}")
             data_path = os.path.join(item_path, f"{item}_confidences.json")
@@ -230,15 +238,10 @@ def show_graph(graph: nx.Graph, folder_path:str):
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         folder_path = os.path.abspath(sys.argv[1])
-        folder_name = os.path.split(folder_path)[1]
         graphs = create_graphs_from_folder(folder_path)
-        merged_graph = merge_graphs(graphs)
-        json_graph.dumps(merged_graph)
+        merged_graph = merge_graphs(graphs, folder_path)
         nodes_with_edges = [node for node, degree in merged_graph.degree() if degree > 0]
         sub_graph = merged_graph.subgraph(nodes_with_edges).copy()
-        show_graph(merged_graph, folder_path)
-        show_circle(merged_graph, folder_path, 'graph')
-        show_circle(sub_graph, folder_path, 'sub_graph')
 
     else:
         print("usage: <script> enter folder_name")
