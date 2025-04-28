0fbcf99e49d92955377f61ac69db0740c1079abc
tsoriki <tsori.kislev@gmail.com>
2025-04-28 08:27:41 +0300

    Auto-commit

--- preprocess/split_fasta_and_run_iupred_on_folder.sh
+++ preprocess/split_fasta_and_run_iupred_on_folder.sh
@@ -12,5 +12,6 @@ awk -v dir="$complex_name/input_fastas" '/^>/{close(out); out=dir"/"substr($0,2)
 # Run IUPred3 on each chain
 for f in "$complex_name"/input_fastas/*.fasta; do
     fname=$(basename "$f" .fasta)
-    python3 ../iupred3/iupred3.py "$f" long > "$complex_name/iupred_outputs/${fname}_iupred.txt"
+    python3 /cs/labs/dina/tsori/af3_example/RedefineSubunit/iupred3/iupred3.py "$f" long > "$complex_name/iupred_outputs/${fname}_iupred.txt"
 done
+  
