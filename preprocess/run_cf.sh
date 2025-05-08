9b6422653ae5beecc548a7210743b5d4cfd69a70
tsoriki <tsori.kislev@gmail.com>
2025-05-08 15:04:00 +0300

    Auto-commit

--- preprocess/run_cf.sh
+++ preprocess/run_cf.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=16G
-#SBATCH --killable
-#SBATCH --requeue
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=40G
+#SBATCH --time=4-00:00:00
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=tsori.kislev@gmail.com
+
+#SBATCH --output=/cs/labs/dina/tsori/af3_example/slurms_outs/cf/%j.out
+
+
+# Check for required arguments
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <COMPLEX_DIR> [MODELS_DIR] [SUBUNITS_INFO_JSON] "
+  exit 1
+fi
+
+
+
+
 
 cd /cs/labs/dina/tsori/af3_example
 source RedefineSubunit/my_venv/bin/activate
