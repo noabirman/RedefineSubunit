a9a4a28c4c5656678ee638b2fdcb80b9b4e5f587
tsori <tsori.kislev@mail.huji.acil>
2025-06-04 16:07:48 +0300

    a

--- partial_fold.py
+++ partial_fold.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from CombFold.scripts.libs.prepare_complex import create_complexes
 
 if __name__ == "__main__":
